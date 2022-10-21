fc5cf900986b6515d2937aa5f5dfe77f837c193e
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-10-21 09:27:07 +0700

    Hotfixing Typo

--- src/Request.php
+++ src/Request.php
@@ -3,14 +3,28 @@
 namespace Nazhim\FirstPackage;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 
 class Request {
 
     public function req(){
-        $client = new Client();
-        
-        $response = $client->request('GET', 'https://jsonplaceholder.typicode.com/posts');
-        
-        return $response;
+        try {
+            // Preparing Base Uri
+            $client = new Client([
+                'base_uri' => 'https://jsonplaceholder.typicode.com'
+            ]);
+            
+            $response = $client->request('GET', '/posts');
+
+            $data = (string) $response->getBody();
+            $data = json_decode($data);
+            
+            return $data;
+        } catch (RequestException $e) {
+            echo $e->getRequest() . "\n";
+            if ($e->hasResponse()) {
+                echo $e->getResponse() . "\n";
+            }
+        }
     }
 }
